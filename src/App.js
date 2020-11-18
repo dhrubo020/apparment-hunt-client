c3d5beaf204d4d70414dbdf34559705cd0ed4ca6
samia07farid <samiafarid07@gmail.com>
2020-11-17 17:34:49 -0800

    done home and detail page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import { createContext, useState } from 'react';
 import AdminLogin from './AdminComponent/AdminLogin/AdminLogin';
 import Login from './Login/Login/Login';
+import DetailsPage from './HomePageComponent/DetailsPage/DetailsPage';
+import Home from './HomePageComponent/Home/Home';
 import PrivateRoute from './Login/PrivateRoute/PrivateRoute';
 
 export const UserContext = createContext();
@@ -13,16 +15,19 @@ function App() {
 	return (
 		<div className="">
 			<UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
-			<BrowserRouter>
-				<Switch>
+				<BrowserRouter>
+					<Switch>
+						
+						<Route path='/admin'><Layout /> </Route>
+						<Route path='/adminLogin'> <AdminLogin /> </Route>
+						<Route exact path='/'> <Home /> </Route>
+						<Route path='/home'> <Home /> </Route>
+						<PrivateRoute path="/details/:detailsId/:detailTitle/:detailPrice"> <DetailsPage /> </PrivateRoute>
+						<Route path="/login"> <Login /> </Route>
+						<Route path='*'> <h4> 404 error </h4> </Route>
 
-					<Route path='/admin'><Layout /> </Route>
-					<Route path='/adminLogin'> <AdminLogin/> </Route>
-					<Route path="/login"> <Login/> </Route>
-					<Route path='*'> <h4> 404 error </h4> </Route>
-
-				</Switch>
-			</BrowserRouter>
+					</Switch>
+				</BrowserRouter>
 			</UserContext.Provider>
 		</div>
 	);
